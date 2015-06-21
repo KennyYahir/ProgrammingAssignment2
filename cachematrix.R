f1566fd272503384a55b24d20459de56f24d158e
KennyYahir <kenny.puma1030@gmail.com>
2015-06-20 23:47:02 -0500

    comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,9 @@
-## Put comments here that give an overall description of what your
-## functions do
 
 ## I have thwo functions, one of them creates a special matrix that
 ## sets the inverse of a matrix A if this is not exist yet and
 ## return it if already exist while the other checks the state of
 ## such matrix and compute the inverse just if is needed
 
-## Write a short comment describing this function
 
 # Creates a special matrix
 
@@ -26,14 +23,11 @@ makeCacheMatrix <- function(A = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
-
 # Checks the state of inverse of A
 # If doesn't exist yet compute and return it
 
 cacheSolve <- function(A, ...) {
-	## Return a matrix that is the inverse of 'x'
+	## Return a matrix that is the inverse of 'A'
 
 	A$getInverse() -> inv
     if(!is.null(inv)) {
